1c84beba9372a4293df5f570f7cd7bf3df5320c7
Nkelliny <sitelegion@coolsite.net>
2013-03-07 12:06:36 -0500

    Update songtagger.py

--- songtagger.py
+++ songtagger.py
@@ -1,10 +1,10 @@
 import numpy as np
-data = np.loadtxt('_csv file location_', 'r')
+data = np.loadtxt('_csv file name_', 'r')
 ave_columns = data.sum(axis=0)/(float(data.shape[0]))
 #data.sum()
 
 reader = int_wrapper(reader)
-with open('_csv file location_', 'rb') as f:
+with open('_csv file name_', 'rb') as f:
 
     for row in reader (f):
         sum += int_wrapper(reader)
